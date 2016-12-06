f1238176e4f69109c6135a1554ecffb0a2146915
AShageev <ale-shageev@mail.ru>
2016-12-07 00:31:00 +0300

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+    if (err) {
+        return console.log('Unable to connect to MongoDb Server', err);
+    }
+
+    console.log('Connected to MongoDb server.');
+
+    db.collection('Users').count().then((count) => {
+        console.log(`Users count: ${count}`);
+    })
+});
